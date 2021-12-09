a2f1a32e7e3f26bd89da0a4e60fbc34f5dabdde1
Francisco Caraballo <francisco.caraballo@gmail.com>
2021-12-09 15:33:08 +0100

    Include the name of the variable in the get error message
    This allows for more useful exception messages

--- lib/src/dotenv.dart
+++ lib/src/dotenv.dart
@@ -51,7 +51,7 @@ class DotEnv {
 
   String get(String name, {String? fallback}) {
     final value = maybeGet(name, fallback: fallback);
-    assert(value != null, 'A non-null fallback is required for missing entries');
+    assert(value != null, '$name variable not found. A non-null fallback is required for missing entries');
     return value!;
   }
 
